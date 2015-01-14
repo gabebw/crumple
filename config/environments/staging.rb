4d8098ffc674cbd77aa3df2f9f54f69be9a5ff76
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-13 21:04:03 -0500

    Set email host to an actual ENV variable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,5 +3,5 @@ require_relative "production"
 Rails.application.configure do
   # ...
 
-  config.action_mailer.default_url_options = { host: 'ENV.fetch("HOST")' }
+  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
