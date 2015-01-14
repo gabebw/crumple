fef7beeafcde94604745861b5aa79fb5a90661ef
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-13 21:55:54 -0500

    Use "/u/" instead of "/l/" as redirect prefix
    The "l" stood for "link", but in our sans-serif font the "l" is totally
    indistinguishable from the number "1". Since we are using numbers as the
    redirect slug, that makes it hard to tell whether the path is "/l/4" (L-4) or
    "/1/4" (1-4).
    
    Using "/u/" (for "URL") makes the letter immediately distinguishable.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :links, only: [:create, :show]
   root "homes#index"
 
-  get "/l/:id" => "redirects#show", as: :redirect
+  get "/u/:id" => "redirects#show", as: :redirect
 end
