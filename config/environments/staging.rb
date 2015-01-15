51ec628c9d49360de2330e998523396a5f5d0af4
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-15 18:07:45 -0500

    Remove last of the action mailer options

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,5 @@
 require_relative "production"
 
 Rails.application.configure do
-  # ...
-
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  # config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
